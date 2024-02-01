b720cf41d9db0cf72216394ece220784d4d806d5
Bryan Pease <bcp015000@gmail.com>
2024-01-31 21:17:23 -0600

    added definitions used by journal to the default export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Logger } from './Logger';
 
-export {StructuredLog} from './StructuredLog';
+export {type StructuredLog, type LogItem} from './StructuredLog';
+export {type Span} from './Span';
+export {type ConfigurationOptions} from './ConfigurationOptions';
+
 export {Logger}
+export {duration} from './duration';
 export default Logger;

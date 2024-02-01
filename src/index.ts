81297cf9d8cd81a853f685ce69c20299e741d17c
Bryan Pease <bcp015000@gmail.com>
2024-01-31 21:21:42 -0600

    missed one

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Logger } from './Logger';
 export {type StructuredLog, type LogItem} from './StructuredLog';
 export {type Span} from './Span';
 export {type ConfigurationOptions} from './ConfigurationOptions';
+export {type LogOptions} from './LogOptions';
 
 export {Logger}
 export {duration} from './duration';
